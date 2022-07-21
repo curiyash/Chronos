203477149f4ccb98439083ec4b7da430d4b975e5
curiyash <curiyash19@gmail.com>
2022-07-21 20:56:57 +0530

    blank

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
+
+const cors = require('cors');
+app.use(cors({
+    origin: ['https://getpocket.com', 'https://www.google.com/']
+}));
+
 app.use(express.static(path.join(__dirname, 'public')))
-  
+
 app.get('/', (req, res) => res.send('Hello'))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

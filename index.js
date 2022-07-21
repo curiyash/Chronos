6079f42aa68ce65bffad152397985616a97e2463
curiyash <curiyash19@gmail.com>
2022-07-21 20:47:33 +0530

    blank

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+  
+app.get('/', (req, res) => res.send('Hello'))
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('Hello'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
